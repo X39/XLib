0599775ba29918c7845c07b05230c7b371677fa3
Marco.Silipo <m.silipo@web.de>
2015-05-07 21:26:29 +0200

    Updated ModuleClass

--- X39_XLib_Modules/X39_XLib_CivilianPopulation.cpp
+++ X39_XLib_Modules/X39_XLib_CivilianPopulation.cpp
@@ -2,8 +2,8 @@ class X39_XLib_CivilianPopulation: Module_F
 {
 	author = "X39|Cpt. HM Murdock";
 	scope = 2; // Editor visibility; 2 will show it in the menu, 1 will hide it.
-	displayName = "Civilian Population"; // Name displayed in the menu
-	//icon = "\X39_XLib_Modules\Res\X39_XLib_Resupply"; // Map icon. Delete this entry to use the default icon
+	displayName = "Civilian Population - Manager"; // Name displayed in the menu
+	icon = "\X39_XLib_Modules\Res\CivilianModule_Manager"; // Map icon. Delete this entry to use the default icon
 	category = "X39_XLib";
 	side = 7;
 
@@ -15,20 +15,6 @@ class X39_XLib_CivilianPopulation: Module_F
 	isDisposable = 0;
 	class Arguments
 	{
-		class SpawnRange
-		{
-			displayName = "Spawn Range";
-			description = "Which distance should be checked each tick for buildings and roads to spawn civilians in";
-			typeName = "NUMBER";
-			defaultValue = 500;
-		};
-		class DespawnRange
-		{
-			displayName = "Despawn Range";
-			description = "At which distance should the unit be despawned?";
-			typeName = "NUMBER";
-			defaultValue = 500;
-		};
 		class MaxPlayer
 		{
 			displayName = "MaxCivilianPerPlayer";
@@ -64,6 +50,53 @@ class X39_XLib_CivilianPopulation: Module_F
 	};
 	class ModuleDescription: ModuleDescription
 	{
-		description = "Will spawn civilians dynamicly in a given range";		
+		description = "Will spawn civilians dynamicly in a given range<br />Link with CP-Areal to get it working";	
+		sync[] = {"arealModule"};	
+		class arealModule
+		{
+			description = "Defines the used areas for the civilian population module";
+			displayName = "Civilian Population - Areal";
+			icon = "\X39_GM_Insurgency\Res\INS_3.paa";
+			position = 1;
+			direction = 0;
+			optional = 1;
+			duplicate = 1;
+			side = 3;
+		};
 	};
+};
+class X39_XLib_CivilianPopulation: Module_F
+{
+	author = "X39|Cpt. HM Murdock";
+	scope = 2; // Editor visibility; 2 will show it in the menu, 1 will hide it.
+	displayName = "Civilian Population - Areal"; // Name displayed in the menu
+	icon = "\X39_XLib_Modules\Res\CivilianModule_Areal"; // Map icon. Delete this entry to use the default icon
+	category = "X39_XLib";
+	side = 7;
+	
+	functionPriority = 1;
+	isGlobal = 1;
+	isPersistent = 1;
+	isTriggerActivated = 0;
+	isDisposable = 0;
+	class Arguments
+	{
+		class SpawnRange
+		{
+			displayName = "Spawn Range";
+			description = "Which distance should be checked each tick for buildings and roads to spawn civilians in";
+			typeName = "NUMBER";
+			defaultValue = 500;
+		};
+		class DespawnRange
+		{
+			displayName = "Despawn Range";
+			description = "At which distance should the unit be despawned?";
+			typeName = "NUMBER";
+			defaultValue = 500;
+		};
+	};
+	class ModuleDescription: ModuleDescription
+	{
+		description = "See 'Civilian Population - Manager'";	
 };
